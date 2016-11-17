43ddc3863b2bc665abe64012578013bf4004527d
Adrián de la Rosa <Adrian.delaRosa@lbigroup.es>
2016-11-17 11:13:21 +0100

    Initial commmit

---
+++ pw-screen-reader.js
@@ -0,0 +1,6 @@
+/*globals customElements, Polymer */
+class PWScreenReader extends Polymer.Element {
+  static get is() { return 'pw-screen-reader'; }
+}
+
+customElements.define(PWScreenReader.is, PWScreenReader);
